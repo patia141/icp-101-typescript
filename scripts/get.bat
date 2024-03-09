c84cbe184eb73e92725829866573404990a2aa21
patia141 <patia141@gmail.com>
2024-03-09 16:58:56 +0100

    commit

---
+++ scripts/get.bat
@@ -0,0 +1,1 @@
+curl -X GET http://bkyz2-fmaaa-aaaaa-qaaaq-cai.localhost:8000/messages
---
+++ scripts/post.bat
@@ -0,0 +1,3 @@
+curl -X POST http://bkyz2-fmaaa-aaaaa-qaaaq-cai.localhost:8000/messages \
+ -H "Content-type: application/json" \
+-d '{"title": "todo list", "body": "some important things", "attachmentURL": "url/path/to/some/photo/attachment"}'
